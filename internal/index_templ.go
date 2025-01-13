c96ac7960b2678f9e230444d5a215273a7ff2e28
Andrew Weymes <andrew.weymes@outlook.com>
2025-01-13 22:39:35 +0800

    add base dark and light theme

--- internal/index_templ.go
+++ internal/index_templ.go
@@ -29,7 +29,7 @@ func Index() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html><head><link rel=\"stylesheet\" href=\"/output.css\"></head><body></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html><head><link rel=\"stylesheet\" href=\"/output.css\"></head><body class=\"bg-main text-inverse\"></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
